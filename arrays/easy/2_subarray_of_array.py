194b30466f51464bc9ce0042631ec74576cb6655
César <cesarlpb@gmail.com>
2022-11-25 10:30:23 +0100

    Solución 1 con 2 bucles for
    Solución alternativa con while

--- arrays/easy/2_subarray_of_array.py
+++ arrays/easy/2_subarray_of_array.py
@@ -26,16 +26,50 @@ sub_arr3 = [4, 1]
 # False
 
 #%% Solución 1 - bucles for
+# n y m -> n * m -> orden n * m
+# 2 variables -> espacio no hay tanta carga
+
+# Alternativa para llevar la cuenta del orden -> dict -> {2: 1, 4 : 3}
+    # Desventaja: vamos a tener que usar otro bucle
 
 def es_subarr_valido(arr, sub_arr):
-    # ¿Como podemos tener en cuenta el orden de los números que encontramos?
     contador = 0
-    for el in sub_arr:  # 2, 4
-        for el2 in arr: # 1, 2, 3, 4
-            if el == el2:
+    idx = 0     # índice del último número encontrado
+    for el in sub_arr:  # [2, 4] 
+        for el2 in arr: # [1, 2, 3, 4] -> 1 y 3 -> 3 > 1
+                        # [4, 1] -> 0 > 3 -> False
+            if el == el2 and arr.index(el2) >= idx:
+                idx = arr.index(el2)
+                print(el, el2, idx)
                 contador += 1
                 break
+
     print(contador, len(sub_arr), contador == len(sub_arr))
     return contador == len(sub_arr) # si contador es 2
+#%% Tests iniciales
+es_subarr_valido(arr, sub_arr)   # True
+es_subarr_valido(arr2, sub_arr2) # True
+es_subarr_valido(arr3, sub_arr3) # Debe retornar False
+#%% Solución 2 con while
 
-es_subarr_valido(arr3, sub_arr3)
+
+#%% Solución - Marta
+''' Por cada elemento (value) en el sub_array
+   miramos si encontramos ese elemento en el array.
+   En caso de encontrarlo salimos de ese bucle while con la posición i
+   y buscamos el nuevo elemento(value) a partir de la posición i del anterior.
+   En caso de no encontrarlo el índice será igual a len(arr)
+'''
+def subArr(arr, sub_arr):
+   # si el subArray es menor no buscamos, retornamos false.
+   if len(arr) < len(sub_arr): return False
+   else:
+       i = 0
+       for value in sub_arr:
+           b = False
+           while i < len(arr) and not b:
+               b = (value == arr[i]) # si es igual es true, sino es false
+               i+= 1
+       return b
+
+
