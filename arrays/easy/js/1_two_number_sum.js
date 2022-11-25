750c610443eaa850a8cb94a0978b61e455bafe9c
César <cesarlpb@gmail.com>
2022-11-25 11:34:07 +0100

    Pasamos las soluciones óptimas a JS

--- arrays/easy/js/1_two_number_sum.js
+++ arrays/easy/js/1_two_number_sum.js
@@ -1,1 +1,22 @@
 // Ej. Pasamos la sol 3 a JS
+
+function sumarDosNumeros3(arr, target){
+    arr.sort(function(a, b){return a - b});
+    let izq = 0, suma;
+    der = arr.length - 1;
+    while(izq < der){
+        suma = arr[izq] + arr[der]
+        if(suma === target){
+            return [arr[izq], arr[der]]
+        } 
+        else if (suma < target){ izq++; } 
+        else if (suma > target){ der--; }
+    }
+    return []
+}
+array_de_ejemplo1 = [3, 5, -4, 8, 11, 1, -1, 6]
+suma_objetivo1 = 10
+array_de_ejemplo2 = [3, 5, -4, 8, 11, 1, -1, 6]
+suma_objetivo2 = 20
+sumarDosNumeros3(array_de_ejemplo1, suma_objetivo1) // [-1, 11]
+sumarDosNumeros3(array_de_ejemplo2, suma_objetivo2) // []
--- arrays/easy/js/2_subarray_of_array.js
+++ arrays/easy/js/2_subarray_of_array.js
@@ -1,1 +1,21 @@
 // Ej Pasar sol 2 a JS
+
+function es_subarr_valido2(arr, sub_arr){
+    let i = 0, j = 0;
+    while(i < arr.length && j < sub_arr.length){
+        if( arr[i] === sub_arr[j] ){
+            j++;
+        }
+        i++;
+    }
+    return j === sub_arr.length
+}
+
+let arr = [1, 2, 3, 4], sub_arr = [1, 3, 4];
+let arr2 = [1, 2, 3, 4], sub_arr2 = [2, 4] 
+let arr3 = [1, 2, 3, 4], sub_arr3 = [4, 1]
+
+es_subarr_valido2(arr, sub_arr)   // True
+es_subarr_valido2(arr2, sub_arr2) // True
+es_subarr_valido2(arr3, sub_arr3) // False
+es_subarr_valido2([1, 2, 3, 4, 1], [1, 4, 1]) // True
